40a3b7d099de61329bfa4ae472a302caae810255
cater20 <onyangoketta@gmail.com>
2022-06-14 08:07:55 +0300

    done with assignment

--- index.js
+++ index.js
@@ -19,6 +19,6 @@
 
 //console.log(superbowlWin(record));
 function superbowlWin(record){
-  const myresult = record.find( record => record.result === "W" );
-  return myresult? myresult.year : undefined;
+  const result = record.find( record => record.result === "W" );
+  return result? result.year : undefined;
 }
